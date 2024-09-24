0f83e006d7e34281aa71779a891cb49139b70ec8
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2024-09-24 16:39:37 +0200

    Update cursor.js

--- src/scripts/cursor.js
+++ src/scripts/cursor.js
@@ -1,4 +1,10 @@
 const cursor = document.querySelector('.cursor');
-		document.addEventListener('mousemove', e => {
-			cursor?.setAttribute("style", "top: " + (e.pageY - 10)+"px; left: " + (e.pageX - 10)+"px;")
-		})
+
+// Function to check if the device is desktop
+const isDesktop = () => window.innerWidth > 1024; // Adjust the width as necessary
+
+if (isDesktop()) {
+    document.addEventListener('mousemove', e => {
+        cursor?.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;");
+    });
+}
