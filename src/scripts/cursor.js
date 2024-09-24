bf2ec691d401e020077cfe99218aa723a17d31a0
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2024-09-24 19:26:15 +0200

    Animation tweaks

--- src/scripts/cursor.js
+++ src/scripts/cursor.js
@@ -1,10 +1,10 @@
 const cursor = document.querySelector('.cursor');
 
 // Function to check if the device is desktop
-const isDesktop = () => window.innerWidth > 1024; // Adjust the width as necessary
+const isDesktop = () => window.innerWidth > 1024;
 
 if (isDesktop()) {
     document.addEventListener('mousemove', e => {
-        cursor?.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;");
+        cursor?.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px; visibility: visible;");
     });
 }
