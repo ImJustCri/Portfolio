9e3bcc7aca579fc4ec067e53a9f1d70006b030a7
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2024-09-24 15:39:30 +0200

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://imjustcri.github.io',
+    base: 'portfolio',
+});
