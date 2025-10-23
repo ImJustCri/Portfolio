e8a014a7b1e40760673a3e7e55597f5fc464ae4f
ImJustCri <cricirsicily@proton.me>
2025-10-23 16:10:59 +0200

    Add astro-compress library

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [compress()]
 })
